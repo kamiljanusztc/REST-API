a2d1cae8b88012b023b8277c7e5ae33fe71eb854
Kamil Janusz <kamiljanusztc@gmail.com>
2021-05-06 17:24:23 +0200

    Publish app

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -15,10 +18,14 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes); 
 app.use('/api', seatsRoutes); 
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
